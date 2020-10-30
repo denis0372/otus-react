a0a809551dfb0e2c7e17d53ba0fcd24bd48b0f11
budenniy <denis0372@mail.ru>
2020-10-30 12:33:43 +0300

    components lifecycle

--- src/lesson5/InputText.tsx
+++ src/lesson5/InputText.tsx
@@ -6,17 +6,33 @@ interface Prop {
     placeHolder?: string;
   }
 
+interface State {
+  value?: string;
+}
 
-export class InputText extends Component<Prop> {
+export class InputText extends Component<Prop, State> {
     constructor(props: Prop) {
       super(props);
+
+      this.state = {
+        value : this.props.value
+      }
     }
  
+    componentDidUpdate(prevProps: Prop) {
+
+      // console.log("componentDidUpdate: " + prevProps.value + " -> " + this.props.value);
+
+      if (prevProps.value !== this.props.value) {
+        this.setState({value: this.props.value});
+      }
+    }
+
     render() {
       return (
           <input
             type="text"
-            defaultValue={this.props.value}
+            value={this.state.value}
             placeholder={this.props.placeHolder}
             onChange={this.props.onChangeAction}
           />
--- src/lesson5/LoginForm.tsx
+++ src/lesson5/LoginForm.tsx
@@ -12,22 +12,64 @@ interface State {
 
 export class LoginForm extends Component<Props, State> {
 
+    successLogin: string;
+    successPassw: string;
+
+
     constructor(props: Props) {
         super(props);
+
+        this.state = {
+          login : "",
+          passw : ""
+        }
+
+        this.successLogin = "";
+        this.successPassw = "";
 
         this.handleLoginChange = this.handleLoginChange.bind(this);
         this.handlePasswChange = this.handlePasswChange.bind(this);
         this.loginAction = this.loginAction.bind(this);
     }
 
+    componentDidMount() {
+        /* Вместо получения с сервера*/
+        this.successLogin = "admin";
+        this.successPassw = "qwerty";
+
+        /*подписка на события*/
+        window.addEventListener('resize', this.handleResize);
+
+        this.setState({});
+    }
+
+    componentWillUnmount() {
+      /*отписка от события*/
+      window.removeEventListener('resize', this.handleResize);
+    }
+
+    shouldComponentUpdate(nextState: State) {
+      // console.log("shouldComponentUpdate:" + (nextState.login !== this.state.login || nextState.passw !== this.state.passw));
+      return nextState.login !== this.state.login || nextState.passw !== this.state.passw;
+    }
+
     render() {
+
+        // console.log("Render");
+
         return(
             <div>
-                <InputText placeHolder="Login" onChangeAction={this.handleLoginChange}/>
-                <InputText placeHolder="Password" onChangeAction={this.handlePasswChange}/>
+                <InputText placeHolder="Login" value={this.state.login} onChangeAction={this.handleLoginChange}/>
+                <InputText placeHolder="Password" value={this.state.passw} onChangeAction={this.handlePasswChange}/>
                 <button onClick={this.loginAction}>Login</button>
+                <div>Check: {this.state.login === this.successLogin && this.state.passw === this.successPassw ? "ok" : "wrong"}</div>
             </div>
         );
+    }
+
+
+    handleResize() {
+      console.log("Resize event");
     }
 
     handleLoginChange (event: any) {
@@ -39,6 +81,6 @@ export class LoginForm extends Component<Props, State> {
     };
 
     loginAction() {
-        alert(`Login: ${this.state.login}, passsw: ${this.state.passw}`);
+        alert(`Login: ${this.state.login}, passsw: ${this.state.passw}, result: ${this.state.passw === this.successPassw && this.state.login === this.successLogin}`);
     };
 }
