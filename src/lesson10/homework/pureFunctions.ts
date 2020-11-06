70f97a50feac9c4a690e6f271d2208d6e7ca5274
budenniy <denis0372@mail.ru>
2020-11-06 09:07:53 +0300

    pure functions

--- src/lesson10/homework/pureFunctions.ts
+++ src/lesson10/homework/pureFunctions.ts
@@ -2,18 +2,23 @@
 export type Team = { name: string; score: number };
 
 export const getTopName = (teams: Team[]): string => {
-  //
+  return teams.reduce<Team>((result, team) => result.score < team.score ? team : result, teams[0]).name;
 };
 
 // Задание 2
 export type QsObj = Record<string, string | number | boolean | object>;
 
 export const createQs = (qsObj: QsObj): string => {
-  //
+  return Object.keys(qsObj).reduce((result, key) => result += `${result.length > 0 ? '&' : '?'}${key}=${qsObj[key]}`, "");
 };
 
 // Задание 3
 
 export const parseQs = (qs: string): QsObj => {
-  //
+
+  let result: QsObj = {};
+
+  qs.substring(1).split("&").map((pair) => result[pair.substring(0, pair.indexOf('='))] = pair.substring(pair.indexOf('=') + 1));
+
+  return result;
 };
