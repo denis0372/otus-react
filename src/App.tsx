ac82458f952c18b3c9523a577e81398bc6035fdc
budenniy <denis0372@mail.ru>
2020-12-29 15:47:57 +0300

    react routing

---
+++ src/App.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { LoginScreen } from "@/screens/LoginScreen";
+import { ConditionsScreen } from "@/screens/ConditionsScreen";
+import { NoMatchScreen } from "@/screens/NoMatchScreen";
+import { UserScreen } from "@/screens/UserScreen";
+
+export const App: React.FC<{}> = () => (
+  <Router>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+          <li>
+            <Link to="/conditions">Conditions Editor</Link>
+          </li>
+          <li>
+            <Link to="/user/Denis">Denis</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route path="/login">
+          <LoginScreen />
+        </Route>
+        <Route path="/conditions" render={() => <ConditionsScreen />} />
+        <Route path="/user/:name" component={UserScreen} />
+        <Route path="*">
+          <NoMatchScreen />
+        </Route>
+      </Switch>
+  </Router>
+);
