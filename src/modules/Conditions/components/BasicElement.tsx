20eca86540d108243a2aa50ac43271d8fecbfd2f
budenniy <denis0372@mail.ru>
2021-02-12 10:15:29 +0300

    переделка изменения стилей при наведении мышью на элементы

--- src/modules/Conditions/components/BasicElement.tsx
+++ src/modules/Conditions/components/BasicElement.tsx
@@ -11,11 +11,9 @@ interface Prop {
 
 export const BasicElement: FC<Prop> = ({element, index, onDelete}) => {
 
-  const [mouseOver, onMouseOver] = useState(0);
-
   return (
-    <div className={`editor-block editor-block-${element.type}`} onMouseEnter={() => onMouseOver(1)} onMouseLeave={() => onMouseOver(0)}>{RuleElementNames[element.type]}
-        <div className={`delete-block ${mouseOver === 1 ? 'active' : ''}`}>
+    <div className={`editor-block editor-block-${element.type}`}>{RuleElementNames[element.type]}
+        <div className="delete-block">
           <FontAwesomeIcon icon={faTimes} color="black" onClick={() => onDelete(index)}/>
         </div>
     </div>
--- src/modules/Conditions/components/ComboBoxElement.tsx
+++ src/modules/Conditions/components/ComboBoxElement.tsx
@@ -13,17 +13,15 @@ interface Prop {
 
 export const ComboBoxElement: FC<Prop> = ({element, index, onDelete, onChange, options}) => {
  
-  const [mouseOver, onMouseOver] = useState(0);
-
   return (
-    <div className={`editor-block editor-block-${element.type}`} onMouseEnter={() => onMouseOver(1)} onMouseLeave={() => onMouseOver(0)}>{RuleElementNames[element.type]}
+    <div className={`editor-block editor-block-${element.type}`} >{RuleElementNames[element.type]}
       <select style={{marginInlineStart: 20}} value={element.value || ''} onChange={(event) => {onChange(index, event.target.value);}} >
         {
           options?.map((rec) => 
             <option key={rec.id} value={rec.id}>{rec.value}</option>)
         }
       </select>
-      <div className={`delete-block ${mouseOver === 1 ? 'active' : ''}`}>
+      <div className="delete-block">
           <FontAwesomeIcon icon={faTimes} color="black" onClick={() => onDelete(index)}/>
       </div>
     </div>
